c2763e43d5f77f2f8b98e2e67d9ffd310e57c869
wsk4 <maa.gonzalezva@gmail.com>
2026-04-29 13:39:25 -0400

    first commit

---
+++ src/test/java/com/sanosysalvos/bff_gateway/BffGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanosysalvos.bff_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
