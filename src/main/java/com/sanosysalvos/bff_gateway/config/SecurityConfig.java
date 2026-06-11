5635bd55989e293e78a0639f5841acb67ae16931
wsk4 <maa.gonzalezva@gmail.com>
2026-06-11 15:11:39 -0400

    Update SecurityConfig.java

--- src/main/java/com/sanosysalvos/bff_gateway/config/SecurityConfig.java
+++ src/main/java/com/sanosysalvos/bff_gateway/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
             .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
             .requestMatchers("/actuator/health", "/actuator/health/**").permitAll()
-            .requestMatchers("/dashboard").permitAll()
+            .requestMatchers("/api/bff/v1/dashboard").permitAll()
             .requestMatchers("/mascotas/*").permitAll()
             .anyRequest().authenticated()
         )
